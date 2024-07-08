830f1ccef588ab1258f9495013b1cc05d1ba7f30
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2024-07-07 19:20:10 -0500

    agregue cash en addcard

--- src/user-module/usuario.service.ts
+++ src/user-module/usuario.service.ts
@@ -71,6 +71,9 @@ export class UsuarioService {
                 console.log("Invalid security code");
             }
 
+            if(!cardDto.cash){
+                cardDto.cash = 10000;
+            }
             /*const esValida = luhn.validate(cardDto.cardNumber); 
             if(esValida){
                 console.log("Not a valid card number");
